9b68a2a0f5234c84e26e03281b6d1a6f1015498d
Kasun Aravinda <wmaravinda@gmail.com>
2023-12-13 23:03:25 +0530

    view layer completed

---
+++ src/pos/layered01/PosLayered01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pos.layered01;
+
+import pos.layered01.view.LayoutView;
+
+/**
+ *
+ * @author kasun
+ */
+public class PosLayered01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LayoutView().setVisible(true);
+        
+    }
+    
+}
